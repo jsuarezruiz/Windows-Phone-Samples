a29ca33cf9ec8d0a59520b80fb0a7b73be169dc6
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2015-01-24 21:34:32 +0100

    HubTile Sample

---
+++ HubTile/Ejemplo_HubTile/Ejemplo_HubTile/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace Ejemplo_HubTile
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //HubTileService.FreezeGroup("hubs1");
+        }
+
+        private void HubTile_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            MessageBox.Show(((HubTile)sender).Title);
+        }
+
+        private void pastasTile_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pastas.xaml", UriKind.Relative));
+        }
+    }
+}
